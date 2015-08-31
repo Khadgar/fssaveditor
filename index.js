f00f6f9371d85810bcbe834c4f3047d772ccb8a2
Khadgar <daniel.szaniszlo@gmail.com>
2015-08-31 12:01:42 +0200

    Comment added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var crypto = require('crypto'); //https://nodejs.org/api/crypto.html
 var fs = require('fs');
 
-//Given IV in \assets\bin\Data\Managed\Assembly-CSharp.dll -> StringCipher.cs
-// tu89geji340t89u2 in hex = 7475383967656a693334307438397532
-var iv = new Buffer('7475383967656a693334307438397532', 'hex');
+// How to find the initVector and key
+// Given IV in \assets\bin\Data\Managed\Assembly-CSharp.dll -> StringCipher.cs
+// keysize = 256
+// initVector = tu89geji340t89u2 in hex = 7475383967656a693334307438397532
+// base64 PassPhrase = UGxheWVy -> in UTF8: Player
+// decodeKey = Rfc2898DeriveBytes(PassPhrase,initVectorinHEX).getBytes(keysize/8) -> A7CA9F3366D892C2F0BEF417341CA971B69AE9F7BACCCFFCF43C62D1D7D021F9
 
+var iv = new Buffer('7475383967656a693334307438397532', 'hex');
 var encrypt = function(data) {
-	//Given Key
 	var decodeKey = new Buffer('A7CA9F3366D892C2F0BEF417341CA971B69AE9F7BACCCFFCF43C62D1D7D021F9', 'hex');
 	var cipher = crypto.createCipheriv("aes-256-cbc", decodeKey, iv);
 	var chunks = [];
@@ -34,8 +37,6 @@ var file = fs.readFileSync('Vault1.sav', "utf8");
 //The content is in base64
 //var base64text = new Buffer(file,'base64')
 var base64text = new Buffer(file,'base64')
-
-
 
 //Decrypting
 console.log('Decrypting Vault1.sav');
