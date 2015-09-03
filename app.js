a558ceb484738867a6c27ddf8f6f36eb527522c3
Khadgar <daniel.szaniszlo@gmail.com>
2015-09-03 16:08:09 +0200

    Drag n Drop file upload implemetation started
    AngulaJS, Bower added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var busboy = require('connect-busboy');
 app.use('/scripts', express.static('scripts'));
 app.use('/bower_components', express.static('bower_components'));
+app.use(busboy());
 
 //routes
-require(path.join(__dirname, './routes/index.js')).Index(app);
+require(path.join(__dirname, './routes/index.js')).Index(app,busboy);
 
 
 var server = app.listen(3000, function () {
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,31 @@ var encrypt = require(path.join(__dirname, '../utils/savhandler.js')).encrypt;
 var decrypt = require(path.join(__dirname, '../utils/savhandler.js')).decrypt;
 
 
-var Index = function(app) {
+var Index = function(app, busboy) {
 
     app.get('/', function(req, res) {
+		console.log('index.html');
+        res.sendFile(path.join(__dirname, '../views','index.html'));
+    });
+	
+	app.post('/upload', function(req, res) {
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function(fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+            //temp dir of the files
+            fstream = fs.createWriteStream(path.join(__dirname, '../files/' + filename));
+            file.pipe(fstream);
+			
+            fstream.on('close', function() {
+				console.log('close')
+				//redirect nem megy mert angular nem engedi az url-t megvaltoztatni
+                res.redirect('/content');
+            });
+        });
+    });
+	
+	app.get('/content', function(req, res) {
 		console.log('Reading original: Vault1.sav');
 		var file = fs.readFileSync(path.join(__dirname, '../files/Vault1.sav'), "utf8");
 		// The content is in base64
@@ -21,7 +43,8 @@ var Index = function(app) {
 		console.log('Write encrypted result Vault1.sav');
 		fs.writeFileSync(path.join(__dirname, '../files/NEW_Vault1.sav'), encrypted);
 		
-        res.sendFile(path.join(__dirname, '../views','index.html'));
+		//Elküldi de nem fog az url megvaltozni itt lehetne elkuldeni dekodolt jsont.
+        res.send('NICE CONTENT');
     });
 
 };
