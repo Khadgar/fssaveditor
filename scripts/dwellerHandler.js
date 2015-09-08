0141a3179e56eecba1aa3e3c22909b6d4ebcd7b8
Khadgar <daniel.szaniszlo@gmail.com>
2015-09-08 15:46:18 +0200

    dwellerHandler functions added
    something wrong with dwellers serializeId

--- scripts/dwellerHandler.js
+++ scripts/dwellerHandler.js
@@ -1,11 +1,44 @@
-var getDwellers = function(data) {
-	var dwellerList = [];
-	var dweller = {};
-	for(var i = 0;i<data['dwellers']['dwellers'].length;i++){
-		dweller['serializeId'] = data['dwellers']['dwellers'][i]['serializeId'];
-		dweller['name'] = data['dwellers']['dwellers'][i]['name'];
-		dweller['lastName'] = data['dwellers']['dwellers'][i]['lastName'];
-		dwellerList.push(dweller);
+//Returns every dweller from the Vault with unique id
+//data: entire SAV (decrypted SAV)
+var getDwellers = function (data) {
+	if (data) {
+		var dwellers = {};
+		for (var i = 0; i < data['dwellers']['dwellers'].length; i++) {
+			dwellers[data['dwellers']['dwellers'][i]['serializeId']] = {
+				'name' : data['dwellers']['dwellers'][i]['name'],
+				'lastName' : data['dwellers']['dwellers'][i]['lastName'],
+				'serializeId' : data['dwellers']['dwellers'][i]['serializeId']
+			};
+		}
 	}
-	return dwellerList
+	return dwellers
 };
+
+//Sets a dweller's name and lastName attribute.
+//dwellers: result of getDwellers (all dweller in the vault)
+//id: unique serializeId of the dweller
+//name, lastName: new first name, last name
+var setDwellerName = function (dwellers, id, name, lastName) {
+	
+	if (dwellers[id]) {
+		dwellers[id] = {
+			'name' : name,
+			'lastName' : lastName
+		};
+	}
+};
+
+//Inserts dwellers into data
+//data: entire SAV
+//dwellers: modified dwellers
+var saveDweller = function (data, dwellers) {
+	if (data && dwellers) {
+		for (var i = 0; i < data['dwellers']['dwellers'].length; i++) {
+			if(data['dwellers']['dwellers'][i]['serializeId'] == (i + 1)){
+				data['dwellers']['dwellers'][i]['name'] = dwellers[i + 1]['name'];
+				data['dwellers']['dwellers'][i]['lastName'] = dwellers[i + 1]['lastName'];
+			}
+		}
+	}
+	return data;
+}
--- scripts/pageControllers.js
+++ scripts/pageControllers.js
@@ -4,7 +4,15 @@ app.controller('bodyController',['$scope', function($scope) {
 
 app.controller('contentController',['$scope','VaultData', function($scope,VaultData) {
 	//VaultData.getVault() contains the decrypted sav as a json
-	$scope.message = VaultData.getVault();
-	console.log('/content opened')
-	console.log('Dwellers: ',getDwellers(VaultData.getVault()))
+	var SAVDATA = VaultData.getVault();
+	var dwellers = getDwellers(SAVDATA);
+	console.log('dwellers: ',dwellers)
+	//set new first name and last name to a dweller
+	setDwellerName(dwellers,1,'Dani','Dani');
+	//insert the modified dweller/dwellers into the decrypted SAV
+	VaultData.setVault(saveDweller(SAVDATA,dwellers));
+	//display new dwellers
+	$scope.message = getDwellers(SAVDATA);
+	//display the content of the sav on console
+	console.log('Modded Dwellers: ',VaultData.getVault())
 }]);
